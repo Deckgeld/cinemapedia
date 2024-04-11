1d1c6398bf8c3da3b8b96d90bcaa434a482c0936
Cristian <cristianjbc99@gmail.com>
2024-04-10 19:57:27 -0600

    Fix typo in MovieMapper class

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -6,7 +6,7 @@ class MovieMapper {
       adult: movieDB.adult,
       //Validamos si el backdropPath es vacio para asignar una imagen por defecto
       backdropPath: (movieDB.backdropPath != '' )
-        ? 'https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}' 
+        ? 'https://image.tmdb.org/t/p/w500${movieDB.backdropPath}' 
         : 'https://ih1.redbubble.net/image.1027712254.9762/pp,840x830-pad,1000x1000,f8f8f8.u2.jpg',
       //Es necesario convertir los generos a string para que el modelo de datos sea consistente
       genreIds: movieDB.genreIds.map((e) => e.toString()).toList(),
@@ -17,9 +17,11 @@ class MovieMapper {
       popularity: movieDB.popularity,
       //Validamos si el posterPath es vacio para asignar una imagen por defecto
       posterPath: (movieDB.posterPath != '')
-        ? 'https://image.tmdb.org/t/p/w500/${movieDB.backdropPath}' 
+      //ODIO ESTA LINEA
+        ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}' 
         //Esto no sirve para filtrar las peliculas, dentro de moviedb_datasource.dart
         : 'no-poster',
+      
       releaseDate: movieDB.releaseDate,
       title: movieDB.title,
       video: movieDB.video,
